d8694d4e89f1066ad95843b9008909418204a460
Khang <khangb612@gmail.com>
2020-04-08 03:16:17 +1000

    update: basic UI completed

--- app/src/main/java/com/example/chuckoverflow/MainActivity.java
+++ app/src/main/java/com/example/chuckoverflow/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.chuckoverflow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private ConstraintLayout mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        mLayout = findViewById(R.id.cl_main);
+//        mLayout.setBackgroundResource(R.drawable.default_bg);
     }
+
+
 }
