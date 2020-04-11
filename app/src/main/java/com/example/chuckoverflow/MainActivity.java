24508ef5797683745d23429e3631efd740a93e64
Khang <khangb612@gmail.com>
2020-04-11 17:46:08 +1000

    chore: added comments for readability

--- app/src/main/java/com/example/chuckoverflow/MainActivity.java
+++ app/src/main/java/com/example/chuckoverflow/MainActivity.java
@@ -36,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Link XML elements to their respective Java class variables
         mTitle = findViewById(R.id.tv_prompt);
         mJoke = findViewById(R.id.tv_joke);
         mFavourite = findViewById(R.id.iv_favourite);
@@ -49,33 +51,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //This sets an onClickListener for the refresh button
         mRefresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mJoke.setText("");
-                mProgress.setVisibility(View.VISIBLE);
-                mProgress.setIndeterminate(true);
-                new GetJokeTask().execute();
+                //Handles the generation of new quotes
+                mJoke.setText(""); //before quote is loaded, set text to blank
+                mProgress.setVisibility(View.VISIBLE); //before quote is loaded, show progress spinner
+                mProgress.setIndeterminate(true); //make progress spinner indeterminate
+                new GetJokeTask().execute(); //execute API functions
             }
         });
 
-        mProgress.setVisibility(View.VISIBLE);
-        mProgress.setIndeterminate(true);
-        new GetJokeTask().execute();
+        //Initial loading of the first quote
+        mProgress.setVisibility(View.VISIBLE); //before quote is loaded, show progress spinner
+        mProgress.setIndeterminate(true); //make progress spinner indeterminate
+        new GetJokeTask().execute(); //execute API functions
     }
 
+    // Handles a GetJokeTask on an AsyncTask --> more reliable + code is more readable
     private class GetJokeTask extends AsyncTask<Void, Void, Joke> {
         @Override
         protected Joke doInBackground(Void... voids) {
+            //Try-catch is used as the try block executes code to retrieve an online API
             try {
+                //Retrofit is used to convert the Json class from the API
                 Retrofit retrofit = new Retrofit.Builder()
                         .baseUrl("https://api.chucknorris.io/")
                         .addConverterFactory(GsonConverterFactory.create())
                         .build();
                 JokeService service = retrofit.create(JokeService.class);
-                Call<Joke> jokeCall = service.getJoke();
-                Response<Joke> jokeResponse = jokeCall.execute();
-                joke = jokeResponse.body();
+                Call<Joke> jokeCall = service.getJoke(); //call method
+                Response<Joke> jokeResponse = jokeCall.execute(); //response method
+                joke = jokeResponse.body(); //setting joke equal to Json from the API response
                 return joke;
             }
             catch (IOException e) {
@@ -86,20 +94,23 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onPostExecute(Joke joke){
-            mProgress.setIndeterminate(false);
-            mProgress.clearAnimation();
-            mProgress.setVisibility(View.GONE);
+            //Post-execute method handles rendering after API is called
+            mProgress.setIndeterminate(false); //ends indeterminate loading
+            mProgress.clearAnimation(); //stops loading animation
+            mProgress.setVisibility(View.GONE); //makes progress bar body invisible
             if(joke != null){
+                //If API function worked this is executed
                 mTitle.setText("Did You Know...");
-                mJoke.setText(joke.getValue());
-                Toast toast = Toast.makeText(activity, "New Joke Generated!", Toast.LENGTH_SHORT);
-                toast.show();
+                mJoke.setText(joke.getValue()); //Quote sentence
+                Toast toast = Toast.makeText(activity, "New Quote Generated!", Toast.LENGTH_SHORT); //UI notification
+                toast.show(); //shows notification
             }
             else {
-                mTitle.setText("Failed to get quote!");
-                mJoke.setText("An error occurred! Please refresh!");
-                Toast toast = Toast.makeText(activity, "Joke Failed To Generate!", Toast.LENGTH_SHORT);
-                toast.show();
+                //If API function failed to retrieve quote, this is executed
+                mTitle.setText("Failed to get quote!"); //Alerts user to failed quote process
+                mJoke.setText("An error occurred! Please refresh!"); //Prompts user how to recover
+                Toast toast = Toast.makeText(activity, "Quote Failed To Generate!", Toast.LENGTH_SHORT); //UI notification
+                toast.show(); //shows notification
             }
         }
     }
